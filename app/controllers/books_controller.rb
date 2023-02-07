0406a7cd28b8157f6ed1e02fedda15106d80aaba
EC2 Default User <ec2-user@ip-172-31-15-26.ap-northeast-1.compute.internal>
2023-02-07 13:27:34 +0000

    [Add]CRUD実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,14 +7,28 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to :books
+    redirect_to book_path(book.id)
   end
 
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to :books
   end
 
   private
